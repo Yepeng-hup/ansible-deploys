afcff31712c4834f9150c278fde1a7f5543678aa
ye <15888520997@sina.cn>
2023-09-05 17:20:54 +0800

    add check env func

--- deploy_all/go/go_role/files/config_go.sh
+++ deploy_all/go/go_role/files/config_go.sh
@@ -2,6 +2,14 @@
 
 DEST_DIR=/usr/local
 
+function check_env(){
+    egrep  "GO|go" /etc/profile | egrep -v "^#" > /dev/null
+    if [ `echo $?` == '0' ];then
+        echo "golang is installd."
+        exit 1
+    fi
+}
+
 function main(){
 cat >> /etc/profile << EMO
 export PATH=$PATH:/usr/local/go/bin
@@ -13,4 +21,5 @@ else
     echo 'go 环境配置失败,请手动检查.'
 fi
 }
+check_env
 main
